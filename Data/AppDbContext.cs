798470c7493583264ea6ca6c4eaaeefba24111df
jtorova5 <jtorova5@gmail.com>
2024-10-10 09:09:08 -0500

    feat: adding Category's model

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,8 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using ProductsOrdersAPI.Models;
 
 namespace ProductsOrdersAPI.Data;
 
 public class AppDbContext : DbContext
 {
+    public DbSet<Category> Categories { get; set; }
+
     public AppDbContext(DbContextOptions options) : base(options) { }
 }
---
+++ Models/Category.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+
+namespace ProductsOrdersAPI.Models;
+
+[Table("categories")]
+public class Category
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    [Column("name")]
+    public required string Name { get; set; }
+
+    [Column("description")]
+    public string? Description { get; set; }
+
+    public Category(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }
+
+    public Category() {}
+}
